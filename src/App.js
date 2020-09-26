8be8a3420bddd0304c34ccf57b5f51e647b58e02
Ogtsn99 <fantasista4123@icloud.com>
2020-09-26 13:40:22 +0900

    jwt認証の確認まで

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import TwitterLogin from 'react-twitter-auth'
+import axios from 'axios'
+// eslint-disable-next-line
+let API_ROOT, CLIENT_ROOT
+if(process.env.NODE_ENV === "development"){
+  API_ROOT = "http://127.0.0.1:3000"
+  CLIENT_ROOT = "http://localhost:4000"
+}else {
+  API_ROOT = "https://animatch-nyan-api.herokuapp.com"
+  CLIENT_ROOT = "https://animatch-nyan.herokuapp.com"
+}
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = { isAuthenticated: false, user: null, token: ''};
+  }
+
+  componentDidMount() {
+    console.log(API_ROOT, CLIENT_ROOT)
+    if(localStorage.getItem('x-auth-token')) {
+      axios.get(API_ROOT + '/api/v1/auth/me', {
+        headers: {
+          'x-auth-token': localStorage.getItem('x-auth-token')
+        }
+      }).then(response => {
+        console.log(response)
+      })
+    }
+  }
+
+  onSuccess = (response) => {
+    const token = response.headers.get('x-auth-token');
+    console.log(token)
+    localStorage.setItem('x-auth-token', token);
+    response.json().then(user => {
+      if (token) {
+        this.setState({isAuthenticated: true, user: user, token: token});
+      }
+    });
+  };
+
+  onFailed = (error) => {
+    alert(error);
+  };
+
+  logout = () => {
+    this.setState({isAuthenticated: false, token: '', user: null})
+  };
+
+  render() {
+    let content = !!this.state.isAuthenticated ?
+      (
+        <div>
+          <p>Authenticated</p>
+          <div>
+            {this.state.user.email}
+          </div>
+          <div>
+            <button onClick={this.logout} className="button" >
+              Log out
+            </button>
+          </div>
+        </div>
+      ) :
+      (
+        <TwitterLogin loginUrl="https://animatch-nyan-api.herokuapp.com/api/v1/auth/twitter"
+                      onFailure={this.onFailed} onSuccess={this.onSuccess}
+                      requestTokenUrl="https://animatch-nyan-api.herokuapp.com/api/v1/auth/twitter/reverse"/>
+      );
+    return (
+      <div className="App">
+        {content}
+      </div>
+    );
+  }
 }
 
 export default App;
